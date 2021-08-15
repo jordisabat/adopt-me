aae0a59c6afe59c62eb0ae8dad31ef92ec21a1e7
Jordi Sabat <jsabat@gmail.com>
2021-08-15 17:51:01 +0200

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import ReactDOM from "react-dom";
 
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me</h1>
-      <Pet name="Lua" animal="Dog" breed="Labrador" />
-      <Pet name="Troc" animal="Dog" breed="Mastin" />
-      <Pet name="Fil" animal="Cat" breed="Common" />
+      <SearchParams />
     </div>
   );
 };
