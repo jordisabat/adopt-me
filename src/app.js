e793a4579df81a4121ec7347fb754a5e4e1994c7
Jordi Sabat <jsabat@gmail.com>
2021-08-15 15:46:11 +0200

    add npm prettier eslint git config

--- src/app.js
+++ src/app.js
@@ -9,9 +9,21 @@ const Pet = (props) => {
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Lua", animal: "Dog", breed: "Labrador" }),
-    React.createElement(Pet, { name: "Troc", animal: "Dog", breed: "Mastin" }),
-    React.createElement(Pet, { name: "Fil", animal: "Cat", breed: "Gat Comu" }),
+    React.createElement(Pet, {
+      name: "Lua",
+      animal: "Dog",
+      breed: "Labrador",
+    }),
+    React.createElement(Pet, {
+      name: "Troc",
+      animal: "Dog",
+      breed: "Mastin",
+    }),
+    React.createElement(Pet, {
+      name: "Fil",
+      animal: "Cat",
+      breed: "Gat Comu",
+    }),
   ]);
 };
 
