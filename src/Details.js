fd197a74c0396a248fb8eba7d5d6e14bb1327609
Jordi Sabat <jsabat@gmail.com>
2021-09-01 17:52:21 +0200

    Error Boundaries

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -21,8 +22,15 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
-      this.state;
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images,
+    } = this.state;
 
     return (
       <div className="details">
@@ -38,4 +46,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
